ea3fd41b2c3c22f793f5ac2a2ac401be855b4e8f
ruk91 <sajithkasn@gmail.com>
2023-12-11 20:52:59 +0100

    Add basic CRUD endpoints

--- Todo.cs
+++ Todo.cs
@@ -2,6 +2,5 @@ public class Todo
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool IsCompleted { get; set; }
+    public bool IsComplete { get; set; }
 }
-```
--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebdbfefaea960fe455f736c1568fdc787a52e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
